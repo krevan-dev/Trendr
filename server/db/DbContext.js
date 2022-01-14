02fc0197631934f054622c79e26f3144e69c1cf1
Kyle Evans <krevan88@protonmail.com>
2022-01-14 12:28:20 -0700

    lunch push

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
+import { CommentSchema } from '../models/Comment';
+import { PostSchema } from '../models/Post';
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
+  Posts = mongoose.model('Post', PostSchema)
+Comments = mongoose.model('Comment',CommentSchema)
 }
 
 export const dbContext = new DbContext()
