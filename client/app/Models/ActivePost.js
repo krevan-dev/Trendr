c06fdd66a32aec6652481d9baac0248391703367
Kyle Evans <krevan88@protonmail.com>
2022-01-14 22:04:33 -0700

    modal added

--- client/app/Models/ActivePost.js
+++ client/app/Models/ActivePost.js
@@ -11,28 +11,24 @@ export class ActivePost {
     return `
         <div class="card p-4 border border-primary border-5">
             <div class="d-flex justify-content-center align-items-center">
-              <button class="btn btn-primary mdi mdi-arrow-left-circle"></button>
               <img src="${this.imgUrl}" alt="${this.title}" class="mx-3 align-items-center" width="60%">
               <div class="d-flex flex-column justify-content-between">
                 <button class="btn btn-success mdi mdi-thumb-up" onclick="app.activePostsController.likes('${this.id}')"></button>
-                <p>${this.likes}</p>
-                <button class="btn btn-primary mdi mdi-arrow-right-circle"></button>
+                <p class="text-center">${this.likes}</p>
                 <button class="btn btn-danger mdi mdi-thumb-down" onclick="app.activePostsController.dislikes('${this.id}')"></button>
-                <p>${this.dislikes}</p>
+                <p class="text-center">${this.dislikes}</p>
               </div>
             </div>
             <div class="d-flex justify-content-center align-items-center card-body">
               <div class="row">
-                <div class="col-12">
-                <img src="${this.profile}" alt"" class="m-2" width="15%"> <p>${this.title}</p>
+                <div class="mx-3 p-3 justify-content-between align-items-center col-8 d-flex">
+                <img src="${this.profile}" alt"" class="m-2" width="15%"><p>${this.title}</p>
+                <button class="btn btn-primary mx-5" onclick="app.commentsController.drawComments()">COMMENTS</button>
+                </div>
+                <div>
                 </div>
               </div>
-            </div>
-            <div class="row card-footer">
-              <div class="mx-3 p-3 justify-content-between col-12 d-flex">${this.time} 
-              <button class="btn btn-primary mx-5" onclick="app.commentsController.drawComments()">COMMENTS</button>
-              </div>
-            </div>
+            </div>            
           </div>
         `
   }
